286b74a33ea8a96a5c19689ab15a0bc0ea8f4436
c1l1mo600 <c1l1mo600@gmail.com>
2022-07-09 18:10:35 +0800

    refactor conn function into util

--- route/conn.py
+++ route/conn.py
@@ -1,7 +1,4 @@
 from quart import Blueprint, render_template, request, websocket
-import os
-import telethon
-import shutil
 from telethon.sync import TelegramClient
 from user.channel.message import incoming_msg
 import response
@@ -9,60 +6,13 @@ import route.util as utils
 
 blueprint = Blueprint("connection", __name__)
 
-api_id = 12655046
-api_hash = 'd84ab8008abfb3ec244630d2a6778fc6'
-
-
-# determine the given phone is valid and return True if client login successfully
-async def login(client, phone) -> bool:
-    if not await client.is_user_authorized():
-        try:
-            await client.send_code_request(phone)
-            context = "please enter the code received in your telegram app"
-            await websocket.send(response.make_response("system", context))
-            code = await websocket.receive()
-            await client.sign_in(phone, code)
-            return True
-        except Exception as e:
-            print(e)
-            return False
-    else:
-        return True
-
-
-async def make_folder(client_id) -> str:
-    path = f'./user/userid{client_id}'
-    if not os.path.exists(path):
-        os.makedirs(path)
-        return ""
-    else:
-        return response.make_response("system", "Error making folder", 500)
-
-
-async def delete_folder(client_id) -> str:
-    path = f"./user/userid{client_id}"
-    print(path)
-    if not os.path.exists(path):
-        return response.make_response("system", "Error deleting folder", 500)
-    else:
-        shutil.rmtree(path, ignore_errors=True)
-        return ""
-
-
-async def find_user(client_list, userID) -> telethon.client:
-    userID = int(userID)
-    if userID in client_list:
-        return client_list[userID]
-    else:
-        return None
-
 
 @blueprint.route("/disconnect")
 async def disconnect():
     userID: str = (request.args.get("user_id"))
-    user = await find_user(utils.client_list, userID)
+    user = await utils.find_user(utils.client_list, userID)
     if user != None:
-        res = await delete_folder(userID)
+        res = await utils.delete_folder(userID)
         if res != "":
             return res
         await user.disconnect()
@@ -75,21 +25,23 @@ async def disconnect():
 async def conn():  # listen on incoming connection
     while True:
         phone = await websocket.receive()
-        client = TelegramClient(phone, api_id, api_hash)
+        client = TelegramClient(phone, utils.api_id, utils.api_hash)
         await client.connect()
-        if await login(client, phone):
+        if await utils.login(client, phone):
             user = await client.get_me()
             # append into client list !!! todo -> might want to use token instead
 
             await websocket.send(response.make_response("system", f"Login as {user.id}"))
             # create folder for further usage
-            res = await make_folder(user.id)
+            res = await utils.make_folder(user.id)
             if res != "":
                 await websocket.send(res)
 
             utils.client_list[user.id] = client
 
-            # load profile & unread message
+            # load profile
+
+            # send unread message count
 
             # listen on message
             incoming_msg.listen_on(utils.client_list, user)
--- route/util.py
+++ route/util.py
@@ -1,5 +1,60 @@
+from quart import Websocket
+import response
+import os
+import shutil
+import telethon
+from typing import Any
+
+
 def init():
-  api_id = 12655046
-  api_hash = 'd84ab8008abfb3ec244630d2a6778fc6'
-  global client_list
-  client_list = dict()
+
+    global client_list, api_hash, api_id
+    api_id = 12655046
+    api_hash = 'd84ab8008abfb3ec244630d2a6778fc6'
+    client_list = dict()
+
+# determine the given phone is valid and return True if client login successfully
+
+
+async def login(client, phone) -> bool:
+    if not await client.is_user_authorized():
+        try:
+            await client.send_code_request(phone)
+            context = "please enter the code received in your telegram app"
+            await websocket.send(response.make_response("system", context))
+            code = await websocket.receive()
+            await client.sign_in(phone, code)
+            return True
+        except Exception as e:
+            print(e)
+            return False
+    else:
+        return True
+
+
+async def make_folder(client_id) -> str:  # create user private folder
+    path = f'./user/userid{client_id}'
+    if not os.path.exists(path):
+        os.makedirs(path)
+        return ""
+    else:
+        return response.make_response("system", "Error making folder", 500)
+
+
+async def delete_folder(client_id) -> str:  # delete user private folder
+    path = f"./user/userid{client_id}"
+    print(path)
+    if not os.path.exists(path):
+        return response.make_response("system", "Error deleting folder", 500)
+    else:
+        shutil.rmtree(path, ignore_errors=True)
+        return ""
+
+
+# find the telethon Client instance
+async def find_user(client_list, userID) -> telethon.client:
+    userID = int(userID)
+    if userID in client_list:
+        return client_list[userID]
+    else:
+        return None
