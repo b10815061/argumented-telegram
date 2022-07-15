f3368e40cf89aa1d02defe390baaa6f4e29c8745
Albert Lu <lualbert88@gmail.com>
2022-07-15 18:31:17 +0800

    Added Code Verification

--- route/conn.py
+++ route/conn.py
@@ -1,10 +1,12 @@
 from pydoc import cli
 from quart import Blueprint, render_template, request, websocket
+from quart_cors import route_cors
 from telethon.sync import TelegramClient
 from user.channel.message import incoming_msg, util
 import response
 import route.util as utils
 import telethon
+import json
 
 blueprint = Blueprint("connection", __name__)
 
@@ -56,17 +58,53 @@ async def a():  # listen on incoming connection
 
 
 @blueprint.post("/login")
+@route_cors(allow_headers=["content-type"],
+            allow_methods=["POST"],
+            allow_origin=["http://localhost:3000"])
 async def login() -> str:  # return userID to frontend
     data = await request.get_json()
     phone = data["phone"]
     client = TelegramClient(phone, utils.api_id, utils.api_hash)
     await client.connect()
+
+    # This line should be added as client can only provide unique phone number
+    utils.client_list[phone] = client
+
     if await utils.has_session(client, phone):
         me = await client.get_me()
         utils.client_list[me.id] = client
         return response.make_response("system", f"Login as {me.id}")
     else:
         return response.make_response("system", "please enter the code received in your telegram app")
+
+@blueprint.post("/verify")
+@route_cors(allow_headers=["content-type"],
+            allow_methods=["POST"],
+            allow_origin=["http://localhost:3000"])
+async def verify():
+    data = await request.get_json()
+    phone = data["phone"]
+    code = data["code"]
+
+    if not await utils.client_list[phone].is_user_authorized():
+        if 'code' in data:
+            try:
+                await utils.client_list[phone].sign_in(phone, code)
+            except:
+                return "Unauthorized", 401
+            me = await utils.client_list[phone].get_me()
+
+            response = {}
+            response["id"] = me.id
+            response["username"] = me.username
+            response["access_hash"] = me.access_hash
+            response["first_name"] = me.first_name
+            response["last_name"] = me.last_name
+            response["phone"] = me.phone
+            json_data = json.dumps(response, ensure_ascii=False)
+
+            return json_data, 200
+    return "Already Logged in", 406
 
 
 @blueprint.websocket("/conn")
