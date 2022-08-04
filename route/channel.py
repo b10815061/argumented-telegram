cb5a3643b2b632500cadb8747bfcacc8a6317bd5
David Shih <david600174@gmail.com>
2022-08-04 15:58:08 +0800

    feat: channel info API
    also refactor setting file

--- route/channel.py
+++ route/channel.py
@@ -37,3 +37,20 @@ async def mute():
         return response.make_response("System", str(channel_id) + str(result))
     else:
         return response.make_response("System", "user not found")
+
+@blueprint.get("/channel/list/<uid>")
+async def channel_list(uid):
+    user: TelegramClient = await utils.find_user(utils.client_list, int(uid))
+    if user == None:
+        return response.make_response("System", "user not found / not login", 404)
+    
+    channelList = []
+    async for d in user.iter_dialogs():
+        channelId = d.entity.id
+        channelName = d.name
+        channelList.append([channelId, channelName])
+        print(f"channel id: {channelId}, channel name: {channelName}")
+        # channelData = await user.get_entity(d.entity.id)
+        # print(d)
+
+    return response.make_response("System", channelList, 200)
