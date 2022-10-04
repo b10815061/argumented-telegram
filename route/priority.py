505f81b2c74c93aac08d6afca07b51b349ee2be8
David Shih <david600174@gmail.com>
2022-10-04 23:30:30 +0800

    fix not existed user set priority problem

--- route/priority.py
+++ route/priority.py
@@ -1,4 +1,5 @@
 from quart import Blueprint, Request, ResponseReturnValue, request
+from telethon import TelegramClient
 from DB.crud import priority
 import route.util as utils
 import response
@@ -16,7 +17,9 @@ output: stringify setting situation, 200
 @blueprint.post("/channel/priority/<id>")
 async def updatePriority(id):
     data = await request.get_json()
-
+    user: TelegramClient = await utils.find_user(utils.client_list, int(id))
+    if user == None:
+        return response.make_response("System", "user not found / not login", 404)
     if not ("channel_id" in data):
         return response.make_response("System", "lack of channel_id", 400)
     if not ("priority" in data):
