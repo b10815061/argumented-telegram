1cbfd40fdadd66184f59bdf7867547750d7d1162
c1l1mo600 <c1l1mo600@gmail.com>
2022-10-19 13:32:06 +0800

    deploy : set up deploy files for ec2

---
+++ scripts/start_docker_compose.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+set -e
+echo $PATH
+# DOCKERPATH=$(whereis docker-compose) 
+# echo $DOCKERPATH
+PATH=$PATH:/usr/local/bin
+echo $PATH
+cd /usr/src/app 
+docker-compose up --build -d
---
+++ scripts/stop_docker_compose.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+set -e
+PATH=$PATH:/usr/local/bin
+cd /usr/src/app
+docker-compose down
