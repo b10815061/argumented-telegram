35c8f6ba633ff363d924484520c1fe26c8393a84
c1l1mo600 <c1l1mo600@gmail.com>
2022-11-27 13:35:09 +0800

    test : trigger deploy push

--- response.py
+++ response.py
@@ -1,12 +1,12 @@
 import string
 import json
 
+
 def make_response(tag, context, code=200) -> string:
     obj = {
         'tag': tag,
         'context': context,
         'code': code
     }
-
     response = json.dumps(obj, ensure_ascii=False)
     return response, code
